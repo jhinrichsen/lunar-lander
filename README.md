c191abbb21b7d4787c0d9ef749465d552f3d17de
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2015-05-25 13:59:45 +0200

    initial checkin

--- README.md
+++
@@ -1,2 +0,0 @@
-# lunar-lander
-Tiny terminal based lunar lander game, ported from the 70s
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("{:^60}", "LUNAR");
+    println!("{:^60}", "CREATIVE COMPUTING MORRISTOWN, NEW JERSEY");
+    println!("\n\n");
+    println!("THIS IS A COMPUTER SIMULATION OF AN APOLLO LUNAR");
+    println!("LANDING CAPSULE.");
+    println!("\n");
+    println!("THE ON-BOARD COMPUTER HAS FAILED (IT WAS MADE BY");
+    println!("XEROX) SO YOU HAVE TO LAND THE CAPSULE MANUALLY.");
+    println!("");
+    println!("SET BURN RATE OF RETRO ROCKETS TO ANY VALUE BETWEEN");
+    println!("0 (FREE FALL) AND 200 (MAXIMUM BURN) POUNDS PER SECOND.");
+    println!("SET NEW BURN RATE EVERY 10 SECONDS.");
+    println!("");
+    println!("CAPSULE WEIGHT 32,500 LBS; FUEL WEIGHT 16,500 LBS.");
+    println!("\n\n");
+    println!("GOOD LUCK");
+}
